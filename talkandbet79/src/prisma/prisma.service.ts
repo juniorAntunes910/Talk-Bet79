6655853562e837adcb7fa730513e6e8620df7171
Junior Antunes <jugabriel2612@gmail.com>
2026-04-27 14:13:35 -0300

    Feat: Bugfix do Bugfix

--- talkandbet79/src/prisma/prisma.service.ts
+++ talkandbet79/src/prisma/prisma.service.ts
@@ -1,8 +1,10 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
-import { PrismaClient} from '../../generated/prisma';
+import { PrismaClient } from '@prisma/client'; // MUDAMOS PARA O PADRÃO AQUI
+
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
-    async onModuleInit(){
-        await this.$connect();
-    }
+  async onModuleInit() {
+    // Se o import acima estiver certo, o $connect funciona
+    await this.$connect();
+  }
 }
